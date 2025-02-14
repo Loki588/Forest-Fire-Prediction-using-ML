8fcbe08ba1aa1d3754c197d01bc660c9e7981526
laukik <laukik2000@gmail.com>
2025-02-14 15:19:23 +0530

    Removed dotenv and switched to Streamlit secrets

--- config.py
+++ config.py
@@ -1,12 +1,7 @@
-from dotenv import load_dotenv
-import os
+import streamlit as st
 
-# Load environment variables from .env file
-load_dotenv()
-
-# Retrieve API keys securely
-OPENWEATHER_API_KEY = os.getenv("OPENWEATHER_API_KEY")
-FIRMS_API_KEY = os.getenv("FIRMS_API_KEY")
-MAILGUN_API_KEY = os.getenv("MAILGUN_API_KEY")
-MAILGUN_DOMAIN = os.getenv("MAILGUN_DOMAIN")
-GOOGLE_MAPS_API_KEY = os.getenv("GOOGLE_MAPS_API_KEY")
+OPENWEATHER_API_KEY = st.secrets["OPENWEATHER_API_KEY"]
+FIRMS_API_KEY = st.secrets["FIRMS_API_KEY"]
+MAILGUN_API_KEY = st.secrets["MAILGUN_API_KEY"]
+MAILGUN_DOMAIN = st.secrets["MAILGUN_DOMAIN"]
+GOOGLE_MAPS_API_KEY =st.secrets["GOOGLE_MAPS_API_KEY"]
